c1e86ccabb528ba4cfc5e65311f0deb89b28992c
Victor Miranda <36968883+VictorMirandaEE@users.noreply.github.com>
2024-10-10 13:21:52 -0300

    Set up project initial organization and metadata. (#11)
    * Sets up the project's initial organization and metadata.
    
    - Adds setup.py to turn the project package installable.
    - Adds __main to turn project module executable.
    - Adds __init__ to define the package folder.
    
    closes #8
    
    * Adds missing new lines at the end of the file.
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Rewards Point System for the Dunder Mifflin Company",
+    author="Victor Miranda",
+    url="https://github.com/VictorMirandaEE/dundie-rewards",
+    packages=find_packages(),
+)
