01591ab51dca1b7c93f7b87a20c8254c2d314b6a
Victor Miranda <36968883+VictorMirandaEE@users.noreply.github.com>
2025-02-03 11:32:53 -0300

    Refactored CLI to use Click and Rich libraries (#20)
    - Added another employee.
    - Refactored CLI to use Click and Rich libraries.
    - Refactored integration test to use built-in test library from Click.

---
+++ integration/constants.py
@@ -0,0 +1,6 @@
+"""Common constants for tests."""
+
+import os
+
+TEST_PATH = os.path.dirname(__file__)
+EMPLOYEES_FILE = os.path.join(TEST_PATH, "../tests", "assets/employees.csv")
--- integration/test_load.py
+++ integration/test_load.py
@@ -1,30 +1,21 @@
 """dundie load subcommand integration test"""
 
-from subprocess import CalledProcessError, check_output
-
 import pytest
+from click.testing import CliRunner
+
+from dundie.cli import load, main
+
+from .constants import EMPLOYEES_FILE
+
+cmd = CliRunner()
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load_positive_call_load_command():
     """Test command load"""
-    output = (
-        check_output(["dundie", "load", "tests/assets/employees.csv"])
-        .decode("utf-8")
-        .split("\n")
-    )
-    assert len(output) == 2
-
-
-def test2():
-    """Test command load"""
-    output = (
-        check_output(["dundie", "load", "tests/assets/employees.csv"])
-        .decode("utf-8")
-        .split("\n")
-    )
-    assert len(output) == 2
+    output = cmd.invoke(load, EMPLOYEES_FILE)
+    assert "Dunder Mifflin Employees" in output.output
 
 
 @pytest.mark.integration
@@ -32,8 +23,6 @@ def test2():
 @pytest.mark.parametrize("wrong_command", ["open", "start", "init"])
 def test_load_negative_call_load_command_with_wrong_parameters(wrong_command):
     """Test command load"""
-    with pytest.raises(CalledProcessError) as error_msg:
-        check_output(
-            ["dundie", wrong_command, "tests/assets/employees.csv"]
-        ).decode("utf-8").split("\n")
-    assert "status 2" in str(error_msg.getrepr())
+    output = cmd.invoke(main, wrong_command, EMPLOYEES_FILE)
+    assert output.exit_code != 0
+    assert f"No such command '{wrong_command}'." in output.output
