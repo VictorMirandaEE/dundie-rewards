04636eca114e93764c5436b79840b36e69d45599
Victor Miranda <victor.miranda.silva@gmail.com>
2025-02-03 09:22:04 -0300

    - Refactored CLI to use Click and Rich libraries. - Refactored integration test to use built in test library from Click.

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,28 +1,49 @@
 """CLI (Command Line Interface) module of dundie"""
 
-import argparse
+import importlib.metadata
 
-from dundie.core import load  # noqa: F401
+import rich_click as click
+from rich.console import Console
+from rich.table import Table
+
+from dundie import core
+
+click.rich_click.USE_RICH_MARKUP = True
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
+click.rich_click.SHOW_METAVARS_COLUMN = False
+click.rich_click.APPEND_METAVARS_HELP = True
 
 
+@click.group()
+@click.version_option(importlib.metadata.version("dundie"))
 def main():
-    parser = argparse.ArgumentParser(
-        description="Dunder Mifflin Rewards CLI",
-        epilog="Enjoy and use with caution.",
-    )
-    parser.add_argument(
-        "subcommand",
-        type=str,
-        help="The subcommand to run",
-        choices=("load", "show", "send", "add"),
-        default="help",
-    )
-    parser.add_argument(
-        "filepath",
-        type=str,
-        help="File path to load",
-        default=None,
-    )
-    args = parser.parse_args()
+    """Dunder Mifflin Rewards System
 
-    print(*globals()[args.subcommand](args.filepath))
+    This is a CLI tool for managing the Dunder Mifflin Rewards program.
+    """
+
+
+@main.command()
+@click.argument("filepath", type=click.Path(exists=True))
+def load(filepath):
+    """Load employees from a CSV file and display them.
+
+    ## Features
+
+    - Validates the data.
+    - Parses the data.
+    - Displays the data.
+    """
+    table = Table(title="Dunder Mifflin Employees")
+    headers = ["Name", "Department", "Role", "e-mail"]
+    for header in headers:
+        table.add_column(header, header_style="magenta", highlight=True)
+
+    employees = core.load(filepath)
+    for employee in employees:
+        table.add_row(*[field.strip() for field in employee.split(",")])
+
+    console = Console()
+    console.print(table)
