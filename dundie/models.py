09abf761c22d63d697d9601f6d0931efd758dd50
Victor Miranda <victor.miranda.silva@gmail.com>
2025-02-27 16:48:52 -0300

    - Updated database model to use Pydantic library for data validation.

--- dundie/models.py
+++ dundie/models.py
@@ -1,72 +1,42 @@
 """Database models for the Dundie app."""
 
-import json
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
 
-from dundie.utils.email import check_valid_email
+from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
 
 
-class InvalidEmailError(Exception):
-    """Custom exception for invalid email addresses."""
-
-    ...
-
-
-class Serializable(ABC):
-    """Abstract base class for serializable objects."""
-
-    @abstractmethod
-    def to_dict(self) -> dict:
-        """Return a dictionary representation of the object."""
-        return vars(self)
-
-    def to_json(self) -> str:
-        """Return a JSON representation of the object."""
-        return json.dumps(self.to_dict())
-
-
-@dataclass
-class Employees(Serializable):
+class Employees(BaseModel):
     """Dataclass for employees."""
 
-    email: str
+    email: EmailStr
     name: str
     department: str
     role: str
-
-    def __post_init__(self):
-        """Validate the email address."""
-        if not check_valid_email(self.email):
-            raise InvalidEmailError(f"Invalid email address: {self.email!r}")
 
     def __str__(self):
         """Return string representation of the employee."""
         return f"{self.name} - {self.role}"
 
-    def to_dict(self) -> dict:
-        """Return a dictionary representation of the employee."""
-        return super().to_dict()  # Override to_dict method
 
-
-@dataclass
-class Balance(Serializable):
+class Balance(BaseModel):
     """Dataclass for employee balance."""
 
     employee: Employees
     points: Decimal
 
-    def to_dict(self):
-        """Return a dictionary representation of the balance."""
-        return {
-            f"{employee.email}": str(self.points),
-        }
+    @field_validator("points", mode="before")
+    @classmethod
+    def points_logic(cls, points: Decimal) -> Decimal:
+        """Validate points."""
+        return points * 2
+
+    model_config = ConfigDict(
+        json_encoders={Employees: lambda employee: employee.name}
+    )
 
 
-@dataclass
-class Transactions(Serializable):
+class Transactions(BaseModel):
     """Dataclass for transactions."""
 
     employee: Employees
@@ -75,19 +45,8 @@ class Transactions(Serializable):
     description: str
     actor: str
 
-    def to_dict(self):
-        """Return a dictionary representation of the transaction."""
-        return {
-            "employee": self.employee.email,
-            "date": self.date.isoformat(),
-            "points": str(self.points),
-            "description": self.description,
-            "actor": self.actor,
-        }
 
-
-@dataclass
-class Users(Serializable):
+class Users(BaseModel):
     """Dataclass for users."""
 
     employee: Employees
@@ -104,18 +63,14 @@ if __name__ == "__main__":
     )
 
     print(employee)
-    print(vars(employee))
-    print(json.dumps(vars(employee)))
-    print(employee.to_dict())
-    print(employee.to_json())
+    print(employee.model_dump())
+    print(employee.model_dump_json())
     print()
 
     balance = Balance(employee=employee, points=Decimal(100))
     print(balance)
-    print(vars(balance))
-    # print(json.dumps(vars(balance)))
-    print(balance.to_dict())
-    print(balance.to_json())
+    print(balance.model_dump())
+    print(balance.model_dump_json())
     print()
 
     transaction = Transactions(
@@ -126,7 +81,6 @@ if __name__ == "__main__":
         actor="system",
     )
     print(transaction)
-    print(vars(transaction))
-    # print(json.dumps(vars(transaction)))
-    print(transaction.to_dict())
-    print(transaction.to_json())
+    print(transaction.model_dump())
+    print(transaction.model_dump_json())
+    print()
